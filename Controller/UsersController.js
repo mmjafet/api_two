b4f30d6881f10aba58ead02ab00a47e76cb4b833
jafet <91506409+mmjafet@users.noreply.github.com>
2025-03-03 22:33:44 -0600

    feat: implement login and signup controllers in UsersController

--- Controller/UsersController.js
+++ Controller/UsersController.js
@@ -15,7 +15,47 @@ const createUserController = async (req, res) => {
     console.log(newUser);
     console.log(username, password);
     console.log(req.body);
-  
+    const { login, signup } = require("../Service/authService");
+
+    const loginController = async (req, res) => {
+        const { username, password } = req.body;
+        //console.log(username, password);
+        try {
+            const respuesta = await login(username, password);
+            
+            if (respuesta.status === 200) {
+                res.status(respuesta.status).json(respuesta);
+            } else {
+                res.status(500).json({ message: "Error interno del servidor 2892198" });
+            }
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    };
+    const signupController = async (req, res) => {
+        const { username, password } = req.body;
+        console.log(username
+            , password
+        );
+        try {
+            const respuesta = await signup(username, password);
+            //console.log(respuesta);
+            if (respuesta.status === 201) {
+                console.log(respuesta);
+                res.status(respuesta.status).json(respuesta);
+            } else {
+                res.status(500).json({ message: "Error interno del servidor23" });
+               
+            }
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+    
+    module.exports = {
+        loginController,
+        signupController
+    };
 }
 
 const loginController = async (req, res) => {
